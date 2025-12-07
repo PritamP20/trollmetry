a400d304d4fb3f2d0fb80a9c9ad0ef14c75316e8
PritamP20 <pripritam7@gmail.com>
2025-12-07 18:40:22 +0530

    changes

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -17,6 +17,12 @@ export async function GET() {
       splashImageUrl: `${APP_URL}/images/splash.png`,
       splashBackgroundColor: "#ffffff",
       webhookUrl: `${APP_URL}/api/webhook`,
+
+      "accountAssociation": {
+        "header": "eyJmaWQiOjEyODA1NDgsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyNkFmMzdBZmJjMzg4MmMyMTM4ZGJBOTcyNWYzOTdCMUE2ZkRGNzA1In0",
+        "payload": "eyJkb21haW4iOiJ0cm9sbG1ldHJ5LnZlcmNlbC5hcHAifQ",
+        "signature": "ojbL+IjBLLYj1M090IlsVzBqrL7JN7wMCiqQDYI5z7hGV5Zaz1UlZMlkUyslhcB8klbHNUOLz6QOFQu9PnkqJxs="
+      }
     },
   };
 
